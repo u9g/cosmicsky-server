cae2e820590ed3d4d3a633f680f80c579b772b05
u9g <git@u9g.dev>
2024-05-26 10:26:58 -0400

    fix the backend

--- index.ts
+++ index.ts
@@ -1,26 +1,37 @@
-Bun.serve<{ username: string; ip: string }>({
+Bun.serve<{ username: string; host: string }>({
   websocket: {
     publishToSelf: true,
     message(ws, message) {
       if (typeof message !== "string") return;
       try {
-        const packet = JSON.parse(message);
+        const packet:
+          | { type: "connected"; username: string; host: string }
+          | { type: "disconnected" }
+          | { type: "ping"; x: number; y: number; z: number } =
+          JSON.parse(message);
+
         console.log(`received packet: ${JSON.stringify(packet)}`);
-        if (packet?.ip?.includes("midnightsky.") === true) {
-          packet.ip = "midnightsky";
+        if (
+          packet.type === "connected" &&
+          packet.host.includes("midnightsky.") === true
+        ) {
+          packet.host = "midnightsky";
         }
 
         switch (packet.type) {
-          case "connected":
-            ws.data = { ip: packet.ip, username: packet.username };
-            ws.subscribe(packet.ip);
+          case "connected": {
+            const { username, host } = packet;
+            ws.data = { host, username };
+            ws.subscribe(ws.data.host);
             break;
-          case "disconnected":
-            ws.unsubscribe(ws.data.ip);
-            ws.data.ip = "";
+          }
+          case "disconnected": {
+            ws.unsubscribe(ws.data.host);
             break;
-          case "ping":
-            if (ws.data.ip === "") {
+          }
+          case "ping": {
+            console.log(`data: ${JSON.stringify(ws.data)}`);
+            if (ws.data.host === "") {
               console.log(
                 `ip is an empty string, so returning early, packet: ${JSON.stringify(
                   packet
@@ -29,20 +40,22 @@ Bun.serve<{ username: string; ip: string }>({
               break;
             }
             const { x, y, z } = packet;
-            const { username } = ws.data;
+            const { username, host } = ws.data;
 
             console.log(`${username} pinged (${x}, ${y}, ${z})`);
 
-            ws.publish(ws.data.ip, JSON.stringify({ x, y, z, username }));
+            ws.publish(host, JSON.stringify({ x, y, z, username }));
 
             break;
-          default:
+          }
+          default: {
             console.log(
               `Unexpected packet of type: ${
-                packet.type
+                (packet as any).type
               }, packet: ${JSON.stringify(packet)}`
             );
             break;
+          }
         }
       } catch (e) {
         console.log(e);
