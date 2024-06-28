2ba3b1774354569579e027cbecb8fa8ef64251a3
u9g <git@u9g.dev>
2024-06-28 13:39:29 -0400

    initial teams implem, missing invites

--- index.ts
+++ index.ts
@@ -6,21 +6,51 @@ const client = new Client({
 });
 await client.connect();
 
+await client.query(`CREATE TABLE IF NOT EXISTS teams (
+	team_id
+		TEXT
+		NOT NULL
+		PRIMARY KEY,
+	owner_uuid
+		STRING
+		NOT NULL
+		UNIQUE
+);`);
+
+await client.query(`CREATE TABLE IF NOT EXISTS team_members (
+	player_uuid
+		STRING
+		NOT NULL
+		PRIMARY KEY,
+	team_id
+		STRING
+		NOT NULL
+		UNIQUE
+);`);
+
 const res = await client.query("SELECT $1::text as message", ["Hello world!"]);
 console.log(res.rows[0].message); // Hello world!
-await client.end();
+// await client.end();
 
-Bun.serve<{ username: string; host: string }>({
+async function uuidFromUsername(username: string): Promise<string> {
+  const response = (await (
+    await fetch(`https://api.ashcon.app/mojang/v2/user/${username}`)
+  ).json()) as any;
+
+  return response.uuid;
+}
+
+Bun.serve<{ username: string; host: string; uuid: string }>({
   websocket: {
     publishToSelf: true,
-    message(ws, message) {
+    async message(ws, message) {
       if (typeof message !== "string") return;
       try {
         const packet:
-          | { type: "connected"; username: string; host: string }
+          | { type: "connected"; username: string; host: string; uuid: string }
           | { type: "disconnected" }
-          | { type: "ping"; x: number; y: number; z: number } =
-          JSON.parse(message);
+          | { type: "ping"; x: number; y: number; z: number }
+          | { type: "createTeam"; teamName: string } = JSON.parse(message);
 
         console.log(`received packet: ${JSON.stringify(packet)}`);
         if (
@@ -32,8 +62,8 @@ Bun.serve<{ username: string; host: string }>({
 
         switch (packet.type) {
           case "connected": {
-            const { username, host } = packet;
-            ws.data = { host, username };
+            const { username, host, uuid } = packet;
+            ws.data = { host, username, uuid };
             ws.subscribe(ws.data.host);
             break;
           }
@@ -41,6 +71,82 @@ Bun.serve<{ username: string; host: string }>({
             ws.unsubscribe(ws.data.host);
             break;
           }
+          case "createTeam": {
+            const uuid = uuidFromUsername(ws.data.username);
+
+            {
+              const teamIds = await client.query(
+                `SELECT owner_uuid FROM teams WHERE team_id = $1;`,
+                [packet.teamName]
+              );
+
+              if (teamIds.rows.length > 0) {
+                ws.publish(
+                  ws.data.uuid,
+                  JSON.stringify({
+                    type: "notification",
+                    message:
+                      "Failed to create team, a team with this name already exists.",
+                  })
+                );
+              }
+            }
+
+            {
+              const teamIds = await client.query(
+                `SELECT team_id FROM team_members WHERE player_uuid = $1;`,
+                [uuid]
+              );
+
+              if (teamIds.rows.length > 0) {
+                ws.publish(
+                  ws.data.uuid,
+                  JSON.stringify({
+                    type: "notification",
+                    message: "Failed to create team, you already have a team.",
+                  })
+                );
+              }
+            }
+
+            await client.query(
+              `INSERT INTO teams (team_id, owner_uuid) VALUES ($1, $2);`,
+              [packet.teamName, ws.data.uuid]
+            );
+            await client.query(
+              `INSERT INTO team_members (player_uuid, team_id) VALUES ($1, $2);`,
+              [ws.data.uuid, packet.teamName]
+            );
+
+            {
+              const teamIds = await client.query(
+                `SELECT team_id FROM team_members WHERE player_uuid = $1;`,
+                [uuid]
+              );
+
+              if (teamIds.rows.length > 0) {
+                ws.publish(
+                  ws.data.uuid,
+                  JSON.stringify({
+                    type: "notification",
+                    message:
+                      "Failed to create team, contact @U9G on discord, code 12.",
+                  })
+                );
+              } else {
+                ws.publish(
+                  ws.data.uuid,
+                  JSON.stringify({
+                    type: "notification",
+                    message: `Created team with name ${packet.teamName}`,
+                  })
+                );
+              }
+            }
+
+            break;
+          }
+
           case "ping": {
             console.log(`data: ${JSON.stringify(ws.data)}`);
             if (ws.data.host === "") {
@@ -51,12 +157,27 @@ Bun.serve<{ username: string; host: string }>({
               );
               break;
             }
-            const { x, y, z } = packet;
-            const { username, host } = ws.data;
 
-            console.log(`${username} pinged (${x}, ${y}, ${z})`);
-
-            ws.publish(host, JSON.stringify({ x, y, z, username }));
+            const teamIds = await client.query(
+              `SELECT team_id FROM team_members WHERE player_uuid = $1;`,
+              [uuidFromUsername(ws.data.username)]
+            );
+            const { username } = ws.data;
+            if (teamIds.rows.length > 0) {
+              const { team_id } = teamIds.rows[0];
+              const { x, y, z } = packet;
+              ws.publish(team_id, JSON.stringify({ x, y, z, username }));
+              console.log(`${username} pinged (${x}, ${y}, ${z})`);
+            } else {
+              ws.publish(
+                username,
+                JSON.stringify({
+                  type: "notification",
+                  message:
+                    "Failed to ping, you don't have a team! Join a team first before pinging.",
+                })
+              );
+            }
 
             break;
           }
